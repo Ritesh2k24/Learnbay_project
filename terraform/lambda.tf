a4aa4a152cdb3c8176407b360da520b80f19cf45
Ritesh2k24 <pawar.ritesh2018@gmail.com>
2025-05-06 19:38:31 +0530

    13th

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -19,20 +19,20 @@ resource "aws_iam_role_policy_attachment" "lambda_dynamo_access" {
 }
 
 resource "aws_lambda_function" "contact_form_handler" {
-  filename         = "contact_handler.zip"
+  filename         = "${path.module}/../lambda/contact_handler.zip"
   function_name    = "contactFormHandler"
   handler          = "contact_handler.lambda_handler"
   runtime          = "python3.9"
   role             = aws_iam_role.lambda_exec.arn
-  source_code_hash = filebase64sha256("${path.module}/lambda/contact_handler.zip")
+  source_code_hash = filebase64sha256("${path.module}/../lambda/contact_handler.zip")
 }
 
 
 resource "aws_lambda_function" "visitor_logger" {
-  filename         = "visitor_logger.zip"
+  filename         = "${path.module}/../lambda/visitor_logger.zip"
   function_name    = "visitorLogger"
   handler          = "visitor_logger.lambda_handler"
   runtime          = "python3.9"
   role             = aws_iam_role.lambda_exec.arn
-  source_code_hash = filebase64sha256("${path.module}/lambda/visitor_logger.zip")
+  source_code_hash = filebase64sha256("${path.module}/../lambda/visitor_logger.zip")
 }
