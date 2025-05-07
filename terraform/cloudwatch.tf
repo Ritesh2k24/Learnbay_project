151735b1ca09a01cb2bce33f25013bd3a887edbb
Ritesh2k24 <pawar.ritesh2018@gmail.com>
2025-05-07 12:29:00 +0530

    22nd

--- terraform/cloudwatch.tf
+++ terraform/cloudwatch.tf
@@ -9,6 +9,16 @@ resource "aws_cloudwatch_log_group" "analytics_lambda_logs" {
   retention_in_days = 14
 }
 
+resource "aws_sns_topic" "alerts" {
+  name = "lambda-alerts-topic"
+}
+
+resource "aws_sns_topic_subscription" "email_alert" {
+  topic_arn = aws_sns_topic.alerts.arn
+  protocol  = "email"
+  endpoint  = "pawar.ritesh2018@gmail.com"
+}
+
 # Alarm: Contact form Lambda errors > 1 in 5 mins
 resource "aws_cloudwatch_metric_alarm" "contact_lambda_errors" {
   alarm_name          = "ContactLambdaErrorAlarm"
@@ -23,6 +33,7 @@ resource "aws_cloudwatch_metric_alarm" "contact_lambda_errors" {
   dimensions = {
     FunctionName = aws_lambda_function.contact_form_handler.function_name
   }
+  alarm_actions = [aws_sns_topic.alerts.arn]
 }
 
 # Alarm: Analytics Lambda errors > 1 in 5 mins
@@ -39,32 +50,4 @@ resource "aws_cloudwatch_metric_alarm" "analytics_lambda_errors" {
   dimensions = {
     FunctionName = aws_lambda_function.visitor_logger.function_name
   }
-}
-
-
-
-resource "aws_sns_topic" "alerts" {
-  name = "lambda-alerts-topic"
-}
-
-resource "aws_sns_topic_subscription" "email_alert" {
-  topic_arn = aws_sns_topic.alerts.arn
-  protocol  = "email"
-  endpoint  = "pawar.ritesh2018@gmail.com"
-}
-
-resource "aws_cloudwatch_metric_alarm" "contact_lambda_errors_secondary" {
-  alarm_name          = "ContactLambdaErrorAlarmSecondary"
-  comparison_operator = "GreaterThanThreshold"
-  evaluation_periods  = 1
-  metric_name         = "Errors"
-  namespace           = "AWS/Lambda"
-  period              = 300
-  statistic           = "Sum"
-  threshold           = 1
-  alarm_description   = "Alarm when contact Lambda function errors > 1 in 5 minutes"
-  dimensions = {
-    FunctionName = aws_lambda_function.contact_form_handler.function_name
-  }
-  alarm_actions = [aws_sns_topic.alerts.arn]
 }
